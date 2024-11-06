241a9cda1176bd58026ecfb59c4b9fbc2aed1633
Edison Toole <edisontoole@gmail.com>
2024-11-06 12:59:40 -0500

    added more cars but broke some price ranges stuff

--- script.js
+++ script.js
@@ -5,85 +5,93 @@ const cars = [
 // Car data organized by type
 const carTypes = {
     SUVs: [
-        { id: 1, image: "x3.jpg", name: "BMW X3", specs: "2.0L Turbo AWD <br>248hp, 258lbft, 4030 lbs<br> $43,700" },
-        { id: 2, image: "q5.jpg", name: "Audi Q5", specs: "2.0L Turbo AWD <br>261hp, 273lbft, 4000 lbs<br> $43,300" },
-        { id: 3, image: "modely.jpg", name: "Tesla Model Y", specs: "Electric AWD <br>384hp, 376lbft, 4416 lbs<br> $52,990" },
-        { id: 4, image: "macan.jpg", name: "Porsche Macan", specs: "2.0L Turbo AWD <br>261hp, 295lbft, 3990 lbs<br> $57,500" },
-        { id: 5, image: "r2.jpg", name: "Rivian R2", specs: "Electric AWD <br>600hp, 600lbft, 5000 lbs<br> $72,500" },
-        { id: 22, image: "CRV.jpg", name: "Honda CRV", specs: "2.0L Hybrid FWD/AWD <br>200hp, 100lbft, 4000 lbs<br> $40,000" },
-        { id: 23, image: "hrv.jpg", name: "Honda HRV", specs: "2.0L FWD/AWD <br>150hp, 100lbft, 3500 lbs<br> $30,000" },
-        { id: 24, image: "corolla_cross.jpg", name: "Toyota Corolla Cross", specs: "1.6L FWD/AWD <br>150hp, 100lbft, 3500 lbs<br> $30,000" },
+        { id: 1, image: "x3.jpg", name: "BMW X3", specs: "2.0L Turbo AWD <br>248 hp, 258 lb-ft, 4030 lbs<br> $43,700", class: "SUV"},
+        { id: 2, image: "q5.jpg", name: "Audi Q5", specs: "2.0L Turbo AWD <br>261 hp, 273 lb-ft, 4000 lbs<br> $43,300", class: "SUV"},
+        { id: 3, image: "modely.jpg", name: "Tesla Model Y", specs: "Electric AWD <br>384 hp, 376 lb-ft, 4416 lbs<br> $52,990", class: "SUV"},
+        { id: 4, image: "macan.jpg", name: "Porsche Macan", specs: "2.0L Turbo AWD <br>261 hp, 295 lb-ft, 3990 lbs<br> $57,500", class: "SUV"},
+        { id: 5, image: "r2.jpg", name: "Rivian R2", specs: "Electric AWD <br>600 hp, 600 lb-ft, 5000 lbs<br> $72,500", class: "SUV"},
+        { id: 22, image: "CRV.jpg", name: "Honda CRV", specs: "2.0L Hybrid FWD/AWD <br>190 hp, 179 lb-ft, 3616   lbs<br> $35,200", class: "SUV"},
+        { id: 23, image: "hrv.jpg", name: "Honda HRV", specs: "2.0L FWD/AWD <br>150 hp, 100 lb-ft, 3500 lbs<br> $30,000", class: "SUV"},
+        { id: 24, image: "corolla_cross.jpg", name: "Toyota Corolla Cross", specs: "1.6L FWD/AWD <br>150 hp, 100 lb-ft, 3500 lbs<br> $30,000", class: "SUV"},
+        { id: 24, image: "lexus_rx.jpg", name: "Lexus RX350", specs: "2.5L FWD/AWD <br>275 hp, 317 lb-ft, 4133 lbs<br> $50,325", class: "SUV"},
     ],
-    // Crossovers: [
-    //     { id: 1, image: "x3.jpg", name: "Honda HRV", specs: "2.0L Turbo AWD <br>248hp, 258lbft, 4030 lbs<br> $43,700" },
-    //     { id: 1, image: "x3.jpg", name: "Toyota Corolla Cross", specs: "2.0L Turbo AWD <br>248hp, 258lbft, 4030 lbs<br> $43,700" },
-
-    // ],
     Sedans: [
-        { id: 6, image: "3series.jpg", name: "BMW 3 Series Sedan", specs: "2.0L Turbo RWD <br>255hp, 294lbft, 3825 lbs<br> $43,800" },
-        { id: 7, image: "is350.jpg", name: "Lexus IS", specs: "3.5L V6 AWD <br>311hp, 280lbft, 3725 lbs<br> $43,050" },
-        { id: 8, image: "a3_sedan.jpg", name: "Audi A3/S3 Sedan", specs: "2.0L Turbo AWD <br>306hp, 295lbft, 3450 lbs<br> $45,000" },
-        { id: 9, image: "ct4.jpg", name: "Cadillac CT4", specs: "2.0L Turbo RWD <br>237hp, 258lbft, 3451 lbs<br> $35,495" },
-        { id: 10, image: "corolla_hybrid.jpg", name: "Toyota Corolla", specs: "1.8L Hybrid FWD <br>121hp, 105lbft, 2850 lbs<br> $24,675" },
-        { id: 11, image: "civic_sedan.jpg", name: "Honda Civic Sedan", specs: "2.0L NA FWD <br>150hp, 177lbft, 2950 lbs<br> $24,250" },
-        { id: 21, image: "camry.jpg", name: "Toyota Camry", specs: "Hybrid FWD/AWD <br>225, 177lbft, 2950 lbs<br> Starting $29,535" },
+        { id: 6, image: "3series.jpg", name: "BMW 3 Series", specs: "2.0L Turbo RWD <br>255 hp, 294 lb-ft, 3825 lbs<br> $43,800", class: "Sedan"},
+        { id: 7, image: "is350.jpg", name: "Lexus IS350", specs: "3.5L V6 RWD <br>311 hp, 280 lb-ft, 3748 lbs<br> $46,735", class: "Sedan"},
+        { id: 8, image: "a3_sedan.jpg", name: "Audi A3/S3 Sedan", specs: "2.0L Turbo AWD <br>306 hp, 295 lb-ft, 3450 lbs<br> $45,000", class: "Sedan"},
+        // { id: 9, image: "ct4.jpg", name: "Cadillac CT4", specs: "2.0L Turbo RWD <br>237 hp, 258 lb-ft, 3451 lbs<br> $35,495", class: "Sedan"},
+        { id: 10, image: "corolla_hybrid.jpg", name: "Toyota Corolla SE", specs: "2.0L FWD <br>168  hp, 151 lb-ft, 2850 lbs<br> $25,750", class: "Sedan"},
+        { id: 11, image: "civic_sedan.jpg", name: "Honda Civic Sport Sedan", specs: "2.0L FWD <br>150 hp, 133 lb-ft, 2926 lbs<br> $27,345", class: "Sedan"},
+        { id: 21, image: "camry.jpg", name: "Toyota Camry SE Hybrid", specs: "Hybrid FWD/AWD <br>225 hp, 163 lb-ft, 3494 lbs<br> Starting $31,835", class: "Sedan"},
     ],
     Hatchbacks: [
-        { id: 12, image: "corolla_hatch.jpg", name: "Toyota Corolla Hatchback", specs: "2.0L FWD <br>169hp, 151lbft, 3060 lbs<br> $23,100" },
-        { id: 13, image: "prius.jpg", name: "Toyota Prius", specs: "2.0L Hybrid AWD <br>196hp, 139lbft, 3375 lbs<br> $27,450" },
-        { id: 14, image: "civic_hatch.jpg", name: "Honda Civic Hatchback", specs: "2.0L NA FWD <br>150hp, 177lbft, 2950 lbs<br> $27,450" },
-        { id: 15, image: "gti.jpg", name: "VW Golf GTI", specs: "2.0L Turbo FWD <br>241hp, 273lbft, 3250 lbs<br> $30,530" }
+        { id: 12, image: "corolla_hatch.jpg", name: "Toyota Corolla Hatchback", specs: "2.0L FWD <br>169 hp, 151 lb-ft, 3060 lbs<br> $23,100", class: "Hatchback"},
+        { id: 14, image: "civic_hatch.jpg", name: "Honda Civic Sport Hatchback", specs: "2.0L NA FWD <br>150 hp, 133 lb-ft, 2976 lbs<br> $28,545", class: "Hatchback"},
+        { id: 15, image: "gti.jpg", name: "VW Golf GTI", specs: "2.0L Turbo FWD <br>241 hp, 273 lb-ft, 3250 lbs<br> $30,530", class: "Hatchback"},
+        { id: 29, image: "mazda3hatch.jpg", name: "Mazda 3 Hatchback", specs: "2.0L Turbo FWD <br>250 hp, 320 lb-ft, 3392 lbs<br> $30,530", class: "Hatchback"},
     ],
-    Performance: [
-        { id: 16, image: "rs3.jpg", name: "Audi RS3 Sedan", specs: "2.5L Turbo AWD <br>401hp, 369lbft, 3680 lbs<br> $62,300" },
-        { id: 18, image: "golfR.jpg", name: "VW Golf R", specs: "2.0L Turbo AWD <br>315hp, 310lbft, 3300 lbs<br> $44,740" },
-        { id: 21, image: "gr86.jpg", name: "Toyota GR86", specs: "2.4L RWD <br>228hp, 184lbft, 2838 lbs<br> $28,400" },
-        { id: 17, image: "grc.jpg", name: "Toyota GR Corolla", specs: "1.6L Turbo AWD <br>300hp, 195lbft, 3276 lbs<br> $46,650" },
-        { id: 19, image: "typeR.jpg", name: "Honda Civic Type R", specs: "2.0L Turbo FWD <br>315hp, 310lbft, 3188lbs<br> $46,690" },
-        { id: 20, image: "integratypes.jpg", name: "Acura Integra Type S", specs: "2.0L Turbo FWD <br>320hp, 310lbft, 3219lbs<br> $53,795" },
-        { id: 20, image: "911.jpg", name: "Porsche 911", specs: "Engine <br>379hp, 331 lbft, 3354lbs<br> $116,050" },
-    ]
+    Hybrids: [
+        { id: 28, image: "civic_hatch.jpg", name: "Honda Civic Sport Hybrid Hatchback", specs: "2.0L Hybrid FWD <br>200 hp, 232 lb-ft, 3258 lbs<br> $31,045", class: "Hatchback"},
+        { id: 13, image: "prius.jpg", name: "Toyota Prius", specs: "2.0L Hybrid FWD <br>194 hp, 139 lb-ft, 3097 lbs<br> $29,085", class: "Hatchback"},
+        { id: 27, image: "civic_sedan.jpg", name: "Honda Civic Sport Hybrid Sedan", specs: "2.0L Hybrid FWD <br>200 hp, 232 lb-ft, 3208 lbs<br> $29,845", class: "Sedan"},
+        { id: 21, image: "camry.jpg", name: "Toyota Camry SE Hybrid", specs: "Hybrid FWD/AWD <br>225 hp, 163 lb-ft, 3494 lbs<br> Starting $31,835", class: "Sedan"},
+        { id: 22, image: "CRV.jpg", name: "Honda CRV", specs: "2.0L Hybrid FWD/AWD <br>190 hp, 179 lb-ft, 3616   lbs<br> $35,200", class: "SUV"},
+        { id: 10, image: "corolla_hybrid.jpg", name: "Toyota Corolla Hybrid SE", specs: "2.0L Hybrid FWD <br>138 hp, 156 lb-ft, 3250 lbs<br> $27,200", class: "Sedan"},
+    ],
+    Performance: [  
+        { id: 16, image: "rs3.jpg", name: "Audi RS3 Sedan", specs: "2.5L Turbo 5-Cyl AWD <br>401 hp, 369 lb-ft, 3680 lbs<br> $62,300", class: "Performance"},
+        { id: 18, image: "golfR.jpg", name: "VW Golf R", specs: "2.0L Turbo AWD <br>315 hp, 310 lb-ft, 3300 lbs<br> $44,740", class: "Performance"},
+        { id: 21, image: "gr86.jpg", name: "Toyota GR86", specs: "2.4L RWD <br>228 hp, 184 lb-ft, 2838 lbs<br> $28,400", class: "Performance"},
+        { id: 17, image: "grc.jpg", name: "Toyota GR Corolla", specs: "1.6L Turbo AWD <br>300 hp, 195 lb-ft, 3276 lbs<br> $46,650", class: "Performance"},
+        { id: 19, image: "typeR.jpg", name: "Honda Civic Type R", specs: "2.0L Turbo FWD <br>315 hp, 310 lb-ft, 3188lbs<br> $46,690", class: "Performance"},
+        { id: 20, image: "integratypes.jpg", name: "Acura Integra Type S", specs: "2.0L Turbo FWD <br>320 hp, 310 lb-ft, 3219lbs<br> $53,795", class: "Performance"},
+        { id: 25, image: "911.jpg", name: "Porsche 911", specs: "Engine <br>379 hp, 331  lb-ft, 3354lbs<br> $116,050", class: "Performance"},
+        { id: 26, image: "supra.jpg", name: "Toyota Supra", specs: "Engine <br>379 hp, 369  lb-ft, 3354lbs<br> $65,000", class: "Performance"},
+        { id: 15, image: "gti.jpg", name: "VW Golf GTI", specs: "2.0L Turbo FWD <br>241 hp, 273 lb-ft, 3250 lbs<br> $30,530", class: "Hatchback"},
+    ], 
 };
 
 const carTypePics = {
     SUVs: {
         name: 'SUV',
         image: 'suv.jpg' // Replace with the actual image path
-    },
+   },
     Sedans: {
         name: 'Sedan',
         image: 'sedan.jpg' // Replace with the actual image path
-    },
+   },
     Hatchbacks: {
         name: 'Hatchback',
         image: 'golf.jpg' // Replace with the actual image path
-    },
-    // Crossovers: {
-    //     name: 'Crossovers',
-    //     image: 'cross.jpg' // Replace with the actual image path
-    // },
+   },
     Performance: {
         name: 'Performance',
         image: 'Performance.jpg' // Replace with the actual image path
+   },
+    Hybrids: {
+        name: 'Hybrids',
+        image: 'cross.jpg' // Replace with the actual image path
     }
 };
 
 const priceRanges = {
     '$20,000 - $40,000': [
-        carTypes.Sedans[4], // Toyota Corolla Hybrid ($24,675)
-        carTypes.Sedans[5], // Honda Civic Hybrid ($26,500)
-        carTypes.Hatchbacks[0], // Toyota Corolla Hatchback ($23,100)
-        carTypes.Hatchbacks[2], // Honda Civic Hatchback ($25,850)
-        carTypes.Hatchbacks[1], // Toyota Prius ($27,450)
-        carTypes.Performance[2], // Toyota GR86 ($28,400)
-        carTypes.Sedans[6]
+        carTypes.Sedans[4], // Toyota Corolla Hybrid 
+        carTypes.Sedans[5], // Honda Civic sedan 
+        carTypes.Hatchbacks[0], // Toyota Corolla Hatchback 
+        carTypes.Hatchbacks[2], // Honda Civic Hatchback 
+        carTypes.Hatchbacks[1], // Toyota Prius 
+        carTypes.Performance[2], // Toyota GR86 
+        carTypes.Sedans[6], //Camry
+        carTypes.Hatchbacks[3], // VW Golf GTI 
+        carTypes.Hatchbacks[5], // civic hybrid hatchback 
+        carTypes.Sedans[7] // civic hybrid sedan
     ],
     '$40,000 - $50,000': [
         carTypes.SUVs[0], // BMW X3 ($43,700)
         carTypes.SUVs[1], // Audi Q5 ($43,300)
         carTypes.Sedans[0], // BMW 3 Series Sedan ($43,800)
         carTypes.Sedans[1], // Lexus IS ($43,050)
-        carTypes.Hatchbacks[3], // VW Golf GTI ($30,530)
         carTypes.Performance[1], // VW Golf R ($44,740)
         carTypes.Performance[3], // Toyota GR Corolla ($46,650)
         carTypes.Performance[4], // Honda Civic Type R ($46,690)
@@ -106,8 +114,6 @@ let currentPairIndex = 0; // Track the pair of cars being compared
 function displaySelections() {
     const cardContainer = document.getElementById("cards");
     cardContainer.innerHTML = ''; 
-
-
     cardContainer.innerHTML += '<h2>Select a Car Type</h2>';
     const carTypeContainer = document.createElement('div');
     carTypeContainer.classList.add('selection-container'); // Add a container for styling
@@ -120,7 +126,7 @@ function displaySelections() {
             <p>${type}</p>
             </div>
         `;
-    }
+   }
     cardContainer.appendChild(carTypeContainer); 
 
     //price range selection
@@ -134,7 +140,7 @@ function displaySelections() {
                 <p>${priceRange}</p>
             </div>
         `;
-    }
+   }
     cardContainer.appendChild(priceRangeContainer); // Append price ranges
 }
 
@@ -175,10 +181,10 @@ function updateBracket() {
                 <img src="${car.image}" alt="${car.name}" width="50">
             `;
             roundDiv.appendChild(carDiv);
-        });
+       });
 
         bracketContainer.appendChild(roundDiv);
-    });
+   });
 }
 
 // Update the `displayPair` and `showFavoriteCar` functions to call updateBracket()
@@ -189,18 +195,18 @@ function displayPair() {
     if (currentRound.length === 1) {
         showFavoriteCar(currentRound[0]);
         return;
-    }
+   }
 
     if (currentPairIndex >= currentRound.length - 1) {
         if (currentRound.length % 2 !== 0) {
             nextRound.push(currentRound[currentRound.length - 1]);
-        }
+       }
         currentRound = nextRound;
         nextRound = [];
         currentPairIndex = 0;
         displayPair();
         return;
-    }
+   }
 
     const car1 = currentRound[currentPairIndex];
     const car2 = currentRound[currentPairIndex + 1];
@@ -219,10 +225,10 @@ function displayPair() {
                 <p>${car2.specs}</p>
             </div>
         `;
-    }
+   }
 
     // Update the bracket with the current state
-    updateBracket();
+    // updateBracket();
 }
 
 function showFavoriteCar(favoriteCar) {
@@ -242,7 +248,7 @@ function showFavoriteCar(favoriteCar) {
     `;
 
     // Show the final car in the bracket
-    updateBracket();
+    // updateBracket();
 }
 
 
@@ -252,7 +258,7 @@ function selectCar(selectedId) {
     const selectedCar = currentRound.find(car => car.id === selectedId);
     if (selectedCar) {
         nextRound.push(selectedCar);
-    }
+   }
 
     // Move to the next pair in the current round
     currentPairIndex += 2;
